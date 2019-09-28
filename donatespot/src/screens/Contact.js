c8162b445101dbbb58a1a2899863cd6a113b8e2a
Rahul Kanojia <irahulkanojia@gmail.com>
2019-09-28 21:25:54 +1000

    Contact Form Done

--- donatespot/src/screens/Contact.js
+++ donatespot/src/screens/Contact.js
@@ -1,28 +1,66 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Alert,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input } from 'react-native-elements';
+import { Input,Button,Text } from 'react-native-elements';
+import axios from 'axios';
 
-<Input
-  placeholder='BASIC INPUT'
-/>
+class Contact extends React.Component {
 
-<Input
-  placeholder='INPUT WITH ICON'
-  leftIcon={{ type: 'font-awesome', name: 'chevron-left' }}
-/>
-
-<Input
-  placeholder='INPUT WITH CUSTOM ICON'
-  leftIcon={
-    <Icon
-      name='user'
-      size={24}
-      color='black'
-    />
+  constructor(props) {
+    super(props);
+    this.state = {name:'',email:'',subject:'',message:''};
   }
-/>
 
-<Input
-  placeholder='INPUT WITH ERROR MESSAGE'
-  errorStyle={{ color: 'red' }}
-  errorMessage='ENTER A VALID ERROR HERE'
-/>
+  render() {
+    return ( 
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Input placeholder='Name' label='Name' onChangeText={(text)=>this.setState({name:text})}/>
+      <Input placeholder='Email' label='Email' onChangeText={(text)=>this.setState({email:text})}/>
+      <Input placeholder='Subject' label='Subject' onChangeText={(text)=>this.setState({subject:text})}/>
+      <Input placeholder='Message' label='Message' onChangeText={(text)=>this.setState({message:text})}/>
+      <Button title="Send Request" onPress={()=>this.callApi(this)}/>
+      </View>
+    );
+  }
+
+  callApi(){
+
+    const formData = new FormData();
+    formData.append('your-name', this.state.name);
+    formData.append('your-email', this.state.email);
+    formData.append('your-subject', this.state.subject);
+    formData.append('your-subject', this.state.subject);
+    axios({
+      url: 'https://donatespot.diplomads.com/wp-json/contact-form-7/v1/contact-forms/50/feedback',
+      method: 'POST',
+      data: formData,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'multipart/form-data'
+  }
+}).then((response) => {
+     if(response.status===200){
+      Alert.alert('Request Sent Successfully');
+      }
+    else{
+     Alert.alert('Failed to send');
+     }
+ })
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+  height:40,
+  borderColor: 'gray',
+  borderWidth:1,
+  width:300
+  }
+});
+
+
+export default Contact;
