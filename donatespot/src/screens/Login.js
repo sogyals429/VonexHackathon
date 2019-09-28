6a92cac5d46d097d57427e25aae77aaee6593435
Sogyal <sogyals429@gmail.com>
2019-09-28 20:24:52 +1000

    login complete

--- donatespot/src/screens/Login.js
+++ donatespot/src/screens/Login.js
@@ -6,13 +6,34 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input,Button,Text } from 'react-native-elements';
-import {axios} from 'axios';
+import axios from 'axios';
 
 class Login extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = {text:''};
-}
+
+  constructor(props) {
+    super(props);
+    this.state = {token:'',username:'',password:''};
+  }
+
+  
+  componentDidMount() {
+    let token;
+
+    axios.post('https://donatespot.diplomads.com/wp-json/jwt-auth/v1/token',{
+      username: 'sogyal',
+      password: 'sogyals429'
+    })
+    .then((response) => {
+      if(response.status===200){
+        token = response.data.token
+        this.setState({
+          token: token
+        })
+      }else{
+        Alert.alert('Failed to get token');
+      }
+    });
+  }
 
   render() {
     return ( 
@@ -23,29 +44,31 @@ constructor(props) {
       //   onChangeText = {text => onChangeText(text)}
       //   value={value}
       //   /> */}
-      <Input placeholder='BASIC INPUT' label='Username' onchange={(text)=>this.setState({text})}/>
-      <Input placeholder='BASIC INPUT' label='Password'/>
-      <Button title="Login" onPress={()=>callApi()}/>
+      <Input placeholder='Username' label='Username' onChangeText={(text)=>this.setState({username:text})}/>
+      <Input placeholder='Password' label='Password' onChangeText={(text)=>this.setState({password:text})}/>
+      <Button title="Login" onPress={()=>this.callApi(this)}/>
       </View>
     );
   }
+
+  callApi(){
+
+    if(state.username != null && state.password != nil){
+      
+    }
+    // console.log(this.state.username)
+    // console.log(this.state.password)
+    // console.log(this.state.token)
+  }
 }
 
-function callApi(){
-var token = axios.post('https://donatespot.diplomads.com/wp-json/jwt-auth/v1/token')
+// function callApi(){
 
-  // fetch('https://donatespot.diplomads.com/wp-json/jwt-auth/v1/token',{
-  //   method: 'POST',
-  //   body:JSON.stringify({
-  //     username:'sogyal',
-  //     password:'sogyals429'
-  //   })
-  // }).then((response) =>response.json()).then((responseJson)=>{
-  //   console.log(responseJson);
-  // }).catch((error) => {
-  //   console.log(error);
-  // })
-}
+  
+//   console.log(response.status);
+  
+//   // data = response.data.data.token;
+// }
 
 
 const styles = StyleSheet.create({
